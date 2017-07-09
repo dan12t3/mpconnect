d4c7da1430d8254ab418158053381c5eb33b1312
dan12t3 <dan12t3@gmail.com>
2017-07-08 20:50:54 -0400

    new feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '@shopify/polaris/styles.css';
+import { CalloutCard } from '@shopify/polaris';
+
+
+// make Component
+
+const App = () => {
+  return(
+    <CalloutCard
+      title="Customize the style of your checkout"
+      illustration="https://cdn.shopify.com/s/assets/admin/checkout/settings-customizecart-705f57c725ac05be5a34ec20c05b94298cb8afd10aac7bd9c7ad02030f48cfa0.svg"
+      primaryAction={{
+        content: 'Customize checkout',
+        url: 'https://www.shopify.com',
+      }}
+    >
+      <p>Upload your store’s logo, change colors and fonts, and more.</p>
+    </CalloutCard>
+
+  );
+}
+
+
+/*
 
 class App extends Component {
   render() {
@@ -17,5 +39,5 @@ class App extends Component {
     );
   }
 }
-
+*/
 export default App;
