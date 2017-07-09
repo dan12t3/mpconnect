aefcad215d74e6c287960dae76fa9a2b882bc7e8
dan12t3 <dan12t3@gmail.com>
2017-07-09 16:59:32 -0400

    connected to backend

--- src/App.js
+++ src/App.js
@@ -6,27 +6,24 @@ import Logo from './logo.svg';
 // make Component
 
 class App extends Component {
-  state = { storeURL: '' };
+  state = { storeName: '' };
 
 
   render(){
 
-    var shop = this.state.storeURL;
-    const api_key = "84f3302b4c6a4c2f3ce6fd4aad2ff99c";
-    const scopes = "read_reports,read_products,read_orders";
-    const redirect_uri = "https://mp-connect.herokuapp.com/";
-    const nonce = "123";
+    var shop = this.state.storeName;
+
 
     return(
       <Banner
         title="Access MPConnect through your Shopify Store"
-        action={{content: 'Continue', url: 'http://localhost:5000/access/store?name='+shop}}
+        action={{content: 'Continue', url: 'https://mpconnectbackend.herokuapp.com/access/store?name='+shop}}
         status="info"
         icon={{ Logo }}
         onDismiss={()=>{}}
       >
       <br />
-        <TextField placeholder="Store URL" value={this.state.storeURL} onChange={(newValue) => this.setState({storeURL: newValue})}/>
+        <TextField placeholder="Store Name" value={this.state.storeName} onChange={(newValue) => this.setState({storeName: newValue})}/>
       </Banner>
 
     );
@@ -34,6 +31,7 @@ class App extends Component {
   }
 }
 
+///sanitize name - storeName
 
 
 /*
