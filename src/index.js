a1756f1ce754f18916286288a22dc548e81c75ca
dan12t3 <dan12t3@gmail.com>
2017-07-29 20:29:18 -0400

    removed polaris embedded app

--- src/index.js
+++ src/index.js
@@ -2,19 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import registerServiceWorker from './registerServiceWorker';
-import {EmbeddedApp} from '@shopify/polaris/embedded';
+//import {EmbeddedApp} from '@shopify/polaris/embedded';
 import { BrowserRouter } from 'react-router-dom';
 import routes from './routes.js';
 import './index.css';
 
 ReactDOM.render(
-  <EmbeddedApp
-  apiKey='84f3302b4c6a4c2f3ce6fd4aad2ff99c'
-  shopOrigin='https://dan12t3devstore.myshopify.com'
-  >
   <BrowserRouter>
   {routes}
   </BrowserRouter>
-
-  </EmbeddedApp>, document.getElementById('root'));
+  , document.getElementById('root'));
 registerServiceWorker();
