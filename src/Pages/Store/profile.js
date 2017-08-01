ffafac44f060e7e4e9219188e43c46beb432d760
dan12t3 <dan12t3@gmail.com>
2017-08-01 00:19:40 -0400

    integrating materializecss

--- src/Pages/Store/profile.js
+++ src/Pages/Store/profile.js
@@ -20,7 +20,7 @@ class Profile extends Component{
     fetch(config.backend+'/profile/fetch',options).then((res) => {
       console.log(res);
       return res.json();
-      
+
     }).then((myJson) => {
       console.log(myJson);
 
@@ -31,7 +31,20 @@ class Profile extends Component{
   }
 
   render(){
-    return(<p>Profile</p>);
+    return(
+      <div className="container">
+        <h1>Profile</h1>
+        <div className="row">
+    <form className="col s12">
+      <div className="row">
+        <div className="input-field col s12">
+          <textarea id="textarea1" className="materialize-textarea"></textarea>
+          <label htmlFor="textarea1">Textarea</label>
+        </div>
+      </div>
+    </form>
+  </div>
+      </div>);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import registerServiceWorker from './registerServiceWorker';
 //import {EmbeddedApp} from '@shopify/polaris/embedded';
 import { BrowserRouter } from 'react-router-dom';
 import routes from './routes.js';
+
+import $ from 'jquery';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.js';
 import './index.css';
 
 ReactDOM.render(
