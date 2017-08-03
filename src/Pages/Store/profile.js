e4007b55be6b8cd280ef8673add0e5057f872dc0
dan12t3 <dan12t3@gmail.com>
2017-08-02 21:37:38 -0400

    update

--- src/Pages/Store/profile.js
+++ src/Pages/Store/profile.js
@@ -16,26 +16,6 @@ class Profile extends Component{
 
   componentWillMount(){
     //fetch request to db
-    const cookie = new Cookie();
-
-    const options ={
-      credentials: 'include',
-      method: 'GET',
-      headers: {
-        'token' : cookie.get('token'),
-        'Content-Type': 'application/json'
-      },
-      mode: 'cors'
-    }
-    fetch(config.backend+'/profile/fetch',options).then((res) => {
-      console.log(res);
-      return res.json();
-
-    }).then((myJson) => {
-      console.log(myJson);
-
-      return;
-    })
 
 
   }
