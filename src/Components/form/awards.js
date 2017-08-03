bcc5b3e6475f294c956bb1f1d4c7768a2014a00c
dan12t3 <dan12t3@gmail.com>
2017-08-03 17:56:22 -0400

    award component makes a fetch request to backend /profile/getProducts

--- src/Components/form/awards.js
+++ src/Components/form/awards.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import config from '../../config.js';
 
-const award = () => {
-  return(
-    <div>
-    <label>Award Winning Products</label>
-    <div className="row">
-    <div className="input-field col s6">
-        <input type="text" id="selectProduct" className="selectProduct" />
-        <label htmlFor="selectProduct">Select Product(s)</label>
-    </div>
-    <div className="input-field col s6">
-      <input type="text" id="productAwards" />
-      <label htmlFor="productAwards">Award(s)</label>
-    </div>
-  </div>
-  </div>
-  );
 
+class award extends Component{
+
+  componentWillMount(){
+
+    const options = {
+      credentials: 'include'
+    }
+
+    fetch(config.backend+'/profile/getProducts',options).then(res => {
+      return res.json();
+    }).then(myJson => {
+      console.log(myJson);
+    });
+
+  }
+
+  render(){
+    return(
+      <div>
+      <label>Award Winning Products</label>
+      <div className="row">
+      <div className="input-field col s6">
+          <input type="text" id="selectProduct" className="selectProduct" />
+          <label htmlFor="selectProduct">Select Product(s)</label>
+      </div>
+      <div className="input-field col s6">
+        <input type="text" id="productAwards" />
+        <label htmlFor="productAwards">Award(s)</label>
+      </div>
+    </div>
+    </div>
+
+    );
+  }
 }
+
 
 export default award;
